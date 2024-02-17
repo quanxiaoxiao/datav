fa5c8b2fb623eba464e24390e45a8450e8cd893f
quan <quan_xiaoxiao@qq.com>
2024-02-17 17:57:31 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,9 @@
 import checkout from './checkout.mjs';
+import getValueOfPathname from './getValueOfPathname.mjs';
+import select from './select/index.mjs';
 
 export {
+  select,
   checkout,
+  getValueOfPathname,
 };
